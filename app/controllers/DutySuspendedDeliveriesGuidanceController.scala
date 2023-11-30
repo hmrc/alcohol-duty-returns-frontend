f2f8aaacc315479e5fef0695506cbdc056a973d2
Jemma Edwards <87577882+JemmaEdwards@users.noreply.github.com>
2023-11-30 17:00:01 +0000

    ADR-795 continue button added

--- app/controllers/DutySuspendedDeliveriesGuidanceController.scala
+++ app/controllers/DutySuspendedDeliveriesGuidanceController.scala
@@ -23,17 +23,17 @@ import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.DutySuspendedDeliveriesGuidanceView
 
-class DutySuspendedDeliveriesGuidanceController @Inject()(
-                                       override val messagesApi: MessagesApi,
-                                       identify: IdentifierAction,
-                                       getData: DataRetrievalAction,
-                                       requireData: DataRequiredAction,
-                                       val controllerComponents: MessagesControllerComponents,
-                                       view: DutySuspendedDeliveriesGuidanceView
-                                     ) extends FrontendBaseController with I18nSupport {
+class DutySuspendedDeliveriesGuidanceController @Inject() (
+  override val messagesApi: MessagesApi,
+  identify: IdentifierAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  val controllerComponents: MessagesControllerComponents,
+  view: DutySuspendedDeliveriesGuidanceView
+) extends FrontendBaseController
+    with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
-    implicit request =>
-      Ok(view())
+  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
+    Ok(view())
   }
 }
