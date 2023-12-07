7ba0f87b81afda5a0f3f57fe42dd2b35dc5b2f70
sini-george <sini.george.9529@gmail.com>
2023-12-07 15:01:04 +0000

    ADR-796 scaffolded initial page

---
+++ test/controllers/DeclareDutySuspendedDeliveriesOutsideUkControllerSpec.scala
@@ -0,0 +1,144 @@
+package controllers
+
+import base.SpecBase
+import forms.DeclareDutySuspendedDeliveriesOutsideUkFormProvider
+import models.{NormalMode, UserAnswers}
+import navigation.{FakeNavigator, Navigator}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import pages.DeclareDutySuspendedDeliveriesOutsideUkPage
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import repositories.SessionRepository
+import views.html.DeclareDutySuspendedDeliveriesOutsideUkView
+
+import scala.concurrent.Future
+
+class DeclareDutySuspendedDeliveriesOutsideUkControllerSpec extends SpecBase with MockitoSugar {
+
+  val formProvider = new DeclareDutySuspendedDeliveriesOutsideUkFormProvider()
+  val form = formProvider()
+
+  def onwardRoute = Call("GET", "/foo")
+
+  val validAnswer = 0
+
+  lazy val declareDutySuspendedDeliveriesOutsideUkRoute = routes.DeclareDutySuspendedDeliveriesOutsideUkController.onPageLoad(NormalMode).url
+
+  "DeclareDutySuspendedDeliveriesOutsideUk Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, declareDutySuspendedDeliveriesOutsideUkRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[DeclareDutySuspendedDeliveriesOutsideUkView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form, NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = UserAnswers(userAnswersId).set(DeclareDutySuspendedDeliveriesOutsideUkPage, validAnswer).success.value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, declareDutySuspendedDeliveriesOutsideUkRoute)
+
+        val view = application.injector.instanceOf[DeclareDutySuspendedDeliveriesOutsideUkView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form.fill(validAnswer), NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+          .overrides(
+            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, declareDutySuspendedDeliveriesOutsideUkRoute)
+            .withFormUrlEncodedBody(("value", validAnswer.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onwardRoute.url
+      }
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, declareDutySuspendedDeliveriesOutsideUkRoute)
+            .withFormUrlEncodedBody(("value", "invalid value"))
+
+        val boundForm = form.bind(Map("value" -> "invalid value"))
+
+        val view = application.injector.instanceOf[DeclareDutySuspendedDeliveriesOutsideUkView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) mustEqual view(boundForm, NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request = FakeRequest(GET, declareDutySuspendedDeliveriesOutsideUkRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery for a POST if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, declareDutySuspendedDeliveriesOutsideUkRoute)
+            .withFormUrlEncodedBody(("value", validAnswer.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+  }
+}
