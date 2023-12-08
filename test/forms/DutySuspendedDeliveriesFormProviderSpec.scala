bb418071ccc5e0487a6119d9f1840343fb55681e
Ewan-James-Donovan <34974028+Ewan-James-Donovan@users.noreply.github.com>
2023-12-08 11:53:25 +0000

    ADR-797 Test classes added in

---
+++ test/forms/DutySuspendedDeliveriesFormProviderSpec.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import forms.behaviours.BigDecimalFieldBehaviours
+import play.api.data.FormError
+
+import scala.collection.immutable.ArraySeq
+
+class DutySuspendedDeliveriesFormProviderSpec extends BigDecimalFieldBehaviours {
+
+  val form = new DutySuspendedDeliveriesFormProvider()()
+
+  ".value" - {
+
+    val fieldName = "duty-suspended-deliveries-input"
+
+    val minimum = 0.01
+    val maximum = 999999999.99
+
+    val validDataGenerator = bigDecimalsInRangeWithCommas(minimum, maximum)
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      validDataGenerator
+    )
+
+    behave like bigDecimalField(
+      form,
+      fieldName,
+      nonNumericError = FormError(fieldName, "dutySuspendedDeliveries.error.nonNumeric"),
+      twoDecimalPlacesError = FormError(fieldName, "dutySuspendedDeliveries.error.twoDecimalPlaces")
+    )
+
+    behave like bigDecimalFieldWithMinimum(
+      form,
+      fieldName,
+      minimum = minimum,
+      expectedError = FormError(fieldName, "dutySuspendedDeliveries.error.minimumRequired", ArraySeq(minimum))
+    )
+
+    behave like bigDecimalFieldWithMaximum(
+      form,
+      fieldName,
+      maximum = maximum,
+      expectedError = FormError(fieldName, "dutySuspendedDeliveries.error.maximumRequired", ArraySeq(999999999.99))
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, "dutySuspendedDeliveries.error.required")
+    )
+  }
+}
