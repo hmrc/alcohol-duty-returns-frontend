2f97a75489cd2206d8a6e67ec21b029c53023a0f
yasaracar <9132533+yasaracar@users.noreply.github.com>
2023-12-18 02:03:41 +0000

    ADR-748 Added Rate models

--- test-utils/generators/ModelGenerators.scala
+++ test-utils/generators/ModelGenerators.scala
@@ -16,8 +16,11 @@
 
 package generators
 
-import models.TaxType
+import models.{AlcoholByVolume, AlcoholRegime, RateBand, RatePeriod, RateType, TaxType}
+import org.scalacheck.Gen.Choose
 import org.scalacheck.{Arbitrary, Gen}
+
+import java.time.YearMonth
 
 trait ModelGenerators {
 
@@ -25,4 +28,82 @@ trait ModelGenerators {
     Arbitrary {
       Gen.oneOf(TaxType.values)
     }
+
+  implicit val arbitraryYearMonth: Arbitrary[YearMonth] = Arbitrary {
+    for {
+      year  <- Gen.choose(1900, 2200)
+      month <- Gen.choose(1, 12)
+    } yield YearMonth.of(year, month)
+  }
+
+  implicit val arbitraryRateType: Arbitrary[RateType] = Arbitrary {
+    Gen.oneOf(RateType.Core, RateType.DraughtRelief)
+  }
+
+  implicit val arbitraryAlcoholRegime: Arbitrary[AlcoholRegime] = Arbitrary {
+    Gen.oneOf(
+      AlcoholRegime.Beer,
+      AlcoholRegime.Cider,
+      AlcoholRegime.Wine,
+      AlcoholRegime.Spirits
+    )
+  }
+
+  val genAlcoholByVolumeValue: Gen[BigDecimal] =
+    for {
+      value <- Gen.choose(0.001, 100.00)
+      scale <- Gen.oneOf(0, 1)
+    } yield BigDecimal(value).setScale(scale, BigDecimal.RoundingMode.UP)
+
+  val genAlcoholByVolumeValueTooBigScale: Gen[BigDecimal] =
+    for {
+      value <- Gen.choose(0.001, 100.00)
+      scale <- Gen.choose(2, 10)
+    } yield BigDecimal(value).setScale(scale, BigDecimal.RoundingMode.UP)
+
+  val genAlcoholByVolumeValueNegative: Gen[BigDecimal] =
+    Gen
+      .chooseNum(Double.MinValue, -0.1)
+      .map(d => BigDecimal(d).setScale(1, BigDecimal.RoundingMode.HALF_UP))
+
+  val genAlcoholByVolumeValueMoreThan100: Gen[BigDecimal] =
+    Gen
+      .chooseNum(100.01, Double.MaxValue)
+      .map(d => BigDecimal(d).setScale(1, BigDecimal.RoundingMode.UP))
+
+  val genAlcoholByVolumeValueOutOfRange: Gen[BigDecimal] =
+    Gen.oneOf(genAlcoholByVolumeValueNegative, genAlcoholByVolumeValueMoreThan100)
+
+  implicit val arbitraryAlcoholByVolume: Arbitrary[AlcoholByVolume] = Arbitrary {
+    genAlcoholByVolumeValue.map(AlcoholByVolume.apply)
+  }
+
+  implicit val chooseBigDecimal: Choose[BigDecimal] =
+    Choose.xmap[Double, BigDecimal](d => BigDecimal(d), bd => bd.toDouble)(implicitly[Choose[Double]])
+
+  implicit val arbitraryRateBand: Arbitrary[RateBand] = Arbitrary {
+    for {
+      taxType       <- Gen.alphaStr
+      description   <- Gen.alphaStr
+      rateType      <- arbitraryRateType.arbitrary
+      alcoholRegime <- Gen.containerOf[Set, AlcoholRegime](arbitraryAlcoholRegime.arbitrary)
+      minABV        <- arbitraryAlcoholByVolume.arbitrary
+      maxABV        <- arbitraryAlcoholByVolume.arbitrary
+      rate          <- Gen.chooseNum(-99999.99, 99999.99).map(BigDecimal(_))
+    } yield RateBand(taxType, description, rateType, alcoholRegime, minABV, maxABV, rate)
+  }
+
+  implicit val arbitraryRatePeriod: Arbitrary[RatePeriod] = Arbitrary {
+    for {
+      name              <- Gen.alphaStr
+      isLatest          <- Gen.oneOf(true, false)
+      validityStartDate <- Arbitrary.arbitrary[YearMonth]
+      validityEndDate   <- Gen.option(Arbitrary.arbitrary[YearMonth])
+      rateBands         <- Gen.nonEmptyListOf(arbitraryRateBand.arbitrary)
+    } yield RatePeriod(name, isLatest, validityStartDate, validityEndDate, rateBands)
+  }
+
+  implicit val arbitraryListRatePeriod: Arbitrary[Seq[RatePeriod]] = Arbitrary {
+    Gen.listOf(arbitraryRatePeriod.arbitrary)
+  }
 }
---
+++ test/models/RatePeriodSpec.scala
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import base.SpecBase
+import generators.ModelGenerators
+import org.scalacheck.Gen
+import org.scalatestplus.mockito.MockitoSugar
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import play.api.libs.json._
+
+import java.time.YearMonth
+
+class RatePeriodSpec extends SpecBase with MockitoSugar with ScalaCheckPropertyChecks with ModelGenerators {
+  "RateType" - {
+    "when writing to json" - {
+      "should return the correct string representation" in {
+        Json.toJson[RateType](RateType.Core) mustEqual JsString("Core")
+        Json.toJson[RateType](RateType.DraughtRelief) mustEqual JsString("DraughtRelief")
+      }
+    }
+    "when reading from json" - {
+      "should translate from the string rep, to the correct case object" in {
+        JsString("Core").as[RateType] mustEqual RateType.Core
+        JsString("DraughtRelief").as[RateType] mustEqual RateType.DraughtRelief
+      }
+      "should return an exception in response to an unrecognised string" in {
+        JsString("some-other").validate[RateType] mustEqual JsError("some-other is not a valid RateType")
+      }
+      "should return a JsError when passed a type that is not a string" in {
+        val result = Json.fromJson[RateType](JsBoolean(true))
+        result mustBe a[JsError]
+      }
+    }
+  }
+
+  "AlcoholRegime" - {
+    "when writing to json" - {
+      "should return the correct string representation" in {
+        Json.toJson[AlcoholRegime](AlcoholRegime.Beer) mustEqual JsString("Beer")
+        Json.toJson[AlcoholRegime](AlcoholRegime.Cider) mustEqual JsString("Cider")
+        Json.toJson[AlcoholRegime](AlcoholRegime.Wine) mustEqual JsString("Wine")
+        Json.toJson[AlcoholRegime](AlcoholRegime.Spirits) mustEqual JsString("Spirits")
+      }
+    }
+    "when reading from json" - {
+      "should translate from the string rep, to the correct case object" in {
+        JsString("Beer").as[AlcoholRegime] mustEqual AlcoholRegime.Beer
+        JsString("Cider").as[AlcoholRegime] mustEqual AlcoholRegime.Cider
+        JsString("Wine").as[AlcoholRegime] mustEqual AlcoholRegime.Wine
+        JsString("Spirits").as[AlcoholRegime] mustEqual AlcoholRegime.Spirits
+      }
+      "should return an exception in response to an unrecognised string" in {
+        JsString("some-other").validate[AlcoholRegime] mustEqual JsError("some-other is not a valid AlcoholRegime")
+      }
+      "should return a JsError when passed a type that is not a string" in {
+        val result = Json.fromJson[AlcoholRegime](JsBoolean(true))
+        result mustBe a[JsError]
+      }
+    }
+  }
+
+  "AlcoholRate" - {
+    "when creating an instance" - {
+      "should successfully save the BigDecimal value when the value is in range" in {
+        forAll(genAlcoholByVolumeValue) { validValue: BigDecimal =>
+          AlcoholByVolume(validValue).value mustEqual validValue
+        }
+      }
+
+      "should throw an exception when the value is not in range" in {
+        forAll(genAlcoholByVolumeValueOutOfRange) { invalidValue: BigDecimal =>
+          val exception = intercept[IllegalArgumentException] {
+            AlcoholByVolume(invalidValue)
+          }
+          exception.getMessage must include("Percentage must be between 0 and 100")
+        }
+      }
+
+      "should throw an exception when the value has more decimal points than 1" in {
+        forAll(genAlcoholByVolumeValueTooBigScale) { invalidValue: BigDecimal =>
+          val exception = intercept[IllegalArgumentException] {
+            AlcoholByVolume(invalidValue)
+          }
+          exception.getMessage must include("Alcohol By Volume must have maximum 1 decimal place")
+        }
+      }
+    }
+
+    "when writing to json" - {
+      "should return the correct number representation with decimal points" in {
+        forAll(genAlcoholByVolumeValue) { validValue =>
+          Json.toJson(AlcoholByVolume(validValue)) mustEqual JsNumber(validValue)
+        }
+      }
+    }
+
+    "when reading from json" - {
+      "should translate from the number representation when it is a valid Alcohol By Volume value" in {
+        forAll(genAlcoholByVolumeValue) { validValue =>
+          JsNumber(validValue).as[AlcoholByVolume] mustEqual AlcoholByVolume(validValue)
+        }
+      }
+
+      "should return an exception in response to an invalid rate value" in {
+        forAll(genAlcoholByVolumeValueTooBigScale) { invalidValue =>
+          JsNumber(invalidValue).validate[AlcoholByVolume] mustBe a[JsError]
+        }
+      }
+
+      "should return a JsError when passed a type that is not a number" in {
+        forAll(Gen.oneOf(Gen.alphaStr, Gen.calendar)) { invalidJsValue =>
+          val result = Json.fromJson[AlcoholByVolume](JsString(invalidJsValue.toString))
+          result mustBe a[JsError]
+        }
+      }
+    }
+  }
+
+  "YearMonth formats" - {
+
+    implicit val yearMonthFormats: Format[YearMonth]               = RatePeriod.yearMonthFormat
+    implicit val optionYearMonthFormats: Format[Option[YearMonth]] = RatePeriod.optionYearMonthFormat
+
+    "should serialise and deserialise YearMonth" in {
+      forAll { (yearMonth: YearMonth) =>
+        val json   = Json.toJson(yearMonth)
+        val result = json.validate[YearMonth]
+        result mustBe a[JsSuccess[_]]
+        result.get mustEqual yearMonth
+      }
+    }
+
+    "should serialise and deserialise Option[YearMonth]" in {
+      forAll { (optionYearMonth: Option[YearMonth]) =>
+        val json   = Json.toJson(optionYearMonth)
+        val result = json.validate[Option[YearMonth]]
+
+        result mustBe a[JsSuccess[_]]
+        result.get mustEqual optionYearMonth
+      }
+    }
+  }
+}
