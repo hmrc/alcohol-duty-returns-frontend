a681eeff2f5f024a7493633f86ed7b33995d4ca3
sini-george <sini.george.9529@gmail.com>
2023-12-20 17:24:00 +0000

    refactored scaffold date page

--- .g8/datePage/generated-test/controllers/$className$ControllerSpec.scala
+++ .g8/datePage/generated-test/controllers/$className$ControllerSpec.scala
@@ -14,7 +14,7 @@ import play.api.inject.bind
 import play.api.mvc.{AnyContentAsEmpty, AnyContentAsFormUrlEncoded, Call}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import repositories.SessionRepository
+import connectors.CacheConnector
 import views.html.$className$View
 
 import scala.concurrent.Future
@@ -77,15 +77,15 @@ class $className$ControllerSpec extends SpecBase with MockitoSugar {
 
     "must redirect to the next page when valid data is submitted" in {
 
-      val mockSessionRepository = mock[SessionRepository]
+      val mockCacheConnector = mock[CacheConnector]
 
-      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+      when(mockCacheConnector.set(any())(any())) thenReturn Future.successful(mock[HttpResponse])
 
       val application =
         applicationBuilder(userAnswers = Some(emptyUserAnswers))
           .overrides(
             bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
-            bind[SessionRepository].toInstance(mockSessionRepository)
+            bind[CacheConnector].toInstance(mockCacheConnector)
           )
           .build()
 
