6a19e3c5d5e1c13e855c4f442846d408e5843b3a
sini-george <sini.george.9529@gmail.com>
2024-01-02 13:21:38 +0000

    ADR-767 initial scaffold page

---
+++ test/forms/DeclareAlcoholDutyQuestionFormProviderSpec.scala
@@ -0,0 +1,29 @@
+package forms
+
+import forms.behaviours.BooleanFieldBehaviours
+import play.api.data.FormError
+
+class DeclareAlcoholDutyQuestionFormProviderSpec extends BooleanFieldBehaviours {
+
+  val requiredKey = "declareAlcoholDutyQuestion.error.required"
+  val invalidKey = "error.boolean"
+
+  val form = new DeclareAlcoholDutyQuestionFormProvider()()
+
+  ".value" - {
+
+    val fieldName = "declareAlcoholDutyQuestion-yesNoValue"
+
+    behave like booleanField(
+      form,
+      fieldName,
+      invalidError = FormError(fieldName, invalidKey)
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
+    )
+  }
+}
