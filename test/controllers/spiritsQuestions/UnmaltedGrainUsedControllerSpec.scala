1112d042a1910bc9ec12de7d88799da97b56973b
Jemma Edwards <87577882+JemmaEdwards@users.noreply.github.com>
2024-01-17 15:04:11 +0000

    ADR-851 scaffold page created in spiritsQuestions section

---
+++ test/controllers/spiritsQuestions/UnmaltedGrainUsedControllerSpec.scala
@@ -0,0 +1,143 @@
+package controllers.spiritsQuestions
+
+import base.SpecBase
+import connectors.CacheConnector
+import controllers.routes
+import forms.spiritsQuestions.UnmaltedGrainUsedFormProvider
+import models.{NormalMode, UserAnswers}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import pages.spiritsQuestions.UnmaltedGrainUsedPage
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+
+import scala.concurrent.Future
+
+class UnmaltedGrainUsedControllerSpec extends SpecBase with MockitoSugar {
+
+  val formProvider = new UnmaltedGrainUsedFormProvider()
+  val form = formProvider()
+
+  def onwardRoute = Call("GET", "/foo")
+
+  val validAnswer = 0
+
+  lazy val unmaltedGrainUsedRoute = routes.UnmaltedGrainUsedController.onPageLoad(NormalMode).url
+
+  "UnmaltedGrainUsed Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, unmaltedGrainUsedRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[UnmaltedGrainUsedView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form, NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = UserAnswers(userAnswersId).set(UnmaltedGrainUsedPage, validAnswer).success.value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, unmaltedGrainUsedRoute)
+
+        val view = application.injector.instanceOf[UnmaltedGrainUsedView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form.fill(validAnswer), NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      val mockCacheConnector = mock[CacheConnector]
+
+      when(mockCacheConnector.set(any())(any())) thenReturn Future.successful(mock[HttpResponse])
+
+      val application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+          .overrides(
+            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+            bind[CacheConnector].toInstance(mockCacheConnector)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, unmaltedGrainUsedRoute)
+            .withFormUrlEncodedBody(("unmaltedGrainUsed-input", validAnswer.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onwardRoute.url
+      }
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, unmaltedGrainUsedRoute)
+            .withFormUrlEncodedBody(("value", "invalid value"))
+
+        val boundForm = form.bind(Map("value" -> "invalid value"))
+
+        val view = application.injector.instanceOf[UnmaltedGrainUsedView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) mustEqual view(boundForm, NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request = FakeRequest(GET, unmaltedGrainUsedRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery for a POST if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, unmaltedGrainUsedRoute)
+            .withFormUrlEncodedBody(("value", validAnswer.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+  }
+}
