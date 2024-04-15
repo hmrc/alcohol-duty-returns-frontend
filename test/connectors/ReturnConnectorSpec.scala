949a6e8386c61898e6a0014972c20e968920feeb
David Flynn <128475768+davidfes@users.noreply.github.com>
2024-04-15 18:10:33 +0100

    WIP ADR-996: Use WireMock is Return Connector tests

--- test/connectors/ReturnConnectorSpec.scala
+++ test/connectors/ReturnConnectorSpec.scala
@@ -16,91 +16,63 @@
 
 package connectors
 
-import base.SpecBase
-import config.FrontendAppConfig
-import models.ReturnError.{ReturnLocked, ReturnNotFound}
+import models.ReturnError.{ReturnLocked, ReturnNotFound, ReturnUpstreamError}
 import models.{Return, ReturnPeriod}
-import org.mockito.ArgumentMatchers.any
-import org.mockito.ArgumentMatchersSugar.eqTo
-import org.mockito.MockitoSugar.{atLeastOnce, mock, verify, when}
-import org.scalatest.concurrent.ScalaFutures
 import play.api.http.Status.{INTERNAL_SERVER_ERROR, LOCKED, NOT_FOUND, OK}
 import play.api.libs.json.Json
-import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpResponse, UpstreamErrorResponse}
 
-import scala.concurrent.{ExecutionContext, Future}
+class ReturnConnectorSpec extends ConnectorBase {
+  protected val endpointName = "alcohol-duty-returns"
 
-class ReturnConnectorSpec extends SpecBase with ScalaFutures {
-
-  protected implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
-  protected implicit val hc: HeaderCarrier    = HeaderCarrier()
-  val mockConfig: FrontendAppConfig           = mock[FrontendAppConfig]
-  val connector                               = new ReturnConnector(config = mockConfig, httpClient = mock[HttpClient])
   val periodKey = "24AE"
   val appaId = "XMADP0000000200"
   val internalId = "12345678"
   val returnPeriod = ReturnPeriod(periodKey, 2024, 5)
   val userReturn = Return(returnPeriod, appaId)
-  val expectedQueryParams = Seq(
-    ("appaId", appaId),
-    ("internalId", internalId),
-    ("periodKey", periodKey)
+  val expectedQueryParams = Map(
+    "appaId" -> appaId,
+    "internalId" -> internalId,
+    "periodKey" -> periodKey
   )
 
   "return connector should" - {
-    "successfully get a return" in {
-      when {
-        connector.httpClient.GET[Either[UpstreamErrorResponse, HttpResponse]](any(), any(), any())(any(), any(), any())
-      } thenReturn Future.successful(Right(HttpResponse(OK, Json.toJson[Return](userReturn).toString())))
-
+    "successfully get a return" in new ConnectorFixture {
+      val url = config.adrReturnGetUrl()
+      stubGetWithParameters(url, expectedQueryParams, OK, Json.toJson(Return(returnPeriod, appaId)).toString)
       whenReady(connector.getReturn(returnPeriod, appaId, internalId).value) {
         result =>
           result mustBe Right(userReturn)
-          verify(connector.httpClient, atLeastOnce)
-            .GET[Either[UpstreamErrorResponse, HttpResponse]](any(), eqTo(expectedQueryParams), any())(any(), any(), any())
-
+          verifyGetWithParameters(url, expectedQueryParams)
       }
     }
 
-    "return ReturnLocked if LOCKED is returned" in {
-      when {
-        connector.httpClient.GET[Either[UpstreamErrorResponse, HttpResponse]](any(), any(), any())(any(), any(), any())
-      } thenReturn Future.successful(Left(UpstreamErrorResponse("", LOCKED)))
-
+    "return ReturnLocked if LOCKED is returned" in new ConnectorFixture {
+      val url = config.adrReturnGetUrl()
+      stubGetWithParameters(url, expectedQueryParams, LOCKED, "")
       whenReady(connector.getReturn(returnPeriod, appaId, internalId).value) {
         result =>
           result mustBe Left(ReturnLocked)
-          verify(connector.httpClient, atLeastOnce)
-            .GET[Either[UpstreamErrorResponse, HttpResponse]](any(), eqTo(expectedQueryParams), any())(any(), any(), any())
-
+          verifyGetWithParameters(url, expectedQueryParams)
       }
     }
 
-    "return ReturnNotFound if NOT_FOUND is returned" in {
-      when {
-        connector.httpClient.GET[Either[UpstreamErrorResponse, HttpResponse]](any(), any(), any())(any(), any(), any())
-      } thenReturn Future.successful(Left(UpstreamErrorResponse("", NOT_FOUND)))
-
+    "return ReturnNotFound if NOT_FOUND is returned" in new ConnectorFixture {
+      val url = config.adrReturnGetUrl()
+      stubGetWithParameters(url, expectedQueryParams, NOT_FOUND, "")
       whenReady(connector.getReturn(returnPeriod, appaId, internalId).value) {
         result =>
           result mustBe Left(ReturnNotFound)
-          verify(connector.httpClient, atLeastOnce)
-            .GET[Either[UpstreamErrorResponse, HttpResponse]](any(), eqTo(expectedQueryParams), any())(any(), any(), any())
-
+          verifyGetWithParameters(url, expectedQueryParams)
       }
     }
 
-    "return error details if anything else is returned" in {
-      when {
-        connector.httpClient.GET[Either[UpstreamErrorResponse, HttpResponse]](any(), any(), any())(any(), any(), any())
-      } thenReturn Future.successful(Right(HttpResponse(INTERNAL_SERVER_ERROR, "")))
-
+    "return error details if anything else is returned" in new ConnectorFixture {
+      val url = config.adrReturnGetUrl()
+      stubGetWithParameters(url, expectedQueryParams, INTERNAL_SERVER_ERROR, "")
       whenReady(connector.getReturn(returnPeriod, appaId, internalId).value) {
         result =>
-          result.isLeft mustBe true
-          verify(connector.httpClient, atLeastOnce)
-            .GET[Either[UpstreamErrorResponse, HttpResponse]](any(), eqTo(expectedQueryParams), any())(any(), any(), any())
-
+          result mustBe Left(ReturnUpstreamError)
+          verifyGetWithParameters(url, expectedQueryParams)
       }
     }
   }
