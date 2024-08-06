1bbdc2b3575a761802f07fad54af7890019c17ea
Michele Satta <3033136+msatta@users.noreply.github.com>
2024-08-06 18:03:12 +0100

    ADR-1272: check if producer has Spirits regime for Quarterly Spirits section pages

--- app/controllers/spiritsQuestions/SpiritTypeController.scala
+++ app/controllers/spiritsQuestions/SpiritTypeController.scala
@@ -38,6 +38,7 @@ class SpiritTypeController @Inject() (
   identify: IdentifierAction,
   getData: DataRetrievalAction,
   requireData: DataRequiredAction,
+  checkSpiritsRegime: CheckSpiritsRegimeAction,
   formProvider: SpiritTypeFormProvider,
   val controllerComponents: MessagesControllerComponents,
   view: SpiritTypeView
@@ -47,14 +48,15 @@ class SpiritTypeController @Inject() (
 
   val form = formProvider()
 
-  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
-    val preparedForm = request.userAnswers.get(SpiritTypePage) match {
-      case None        => form
-      case Some(value) => form.fill(value)
-    }
+  def onPageLoad(mode: Mode): Action[AnyContent] =
+    (identify andThen getData andThen requireData andThen checkSpiritsRegime) { implicit request =>
+      val preparedForm = request.userAnswers.get(SpiritTypePage) match {
+        case None        => form
+        case Some(value) => form.fill(value)
+      }
 
-    Ok(view(preparedForm, mode))
-  }
+      Ok(view(preparedForm, mode))
+    }
 
   def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
     implicit request =>
