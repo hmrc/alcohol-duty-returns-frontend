71cbc437f440f2721f350803a61a53646f34151e
Michele Satta <3033136+msatta@users.noreply.github.com>
2024-09-17 12:30:03 +0100

    ADR-1144: Adds locking release for Return-to-BTA link

---
+++ app/controllers/BusinessTaxAccountRedirect.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.Constants.periodKeySessionKey
+import config.FrontendAppConfig
+import connectors.CacheConnector
+import controllers.actions.{DataRetrievalAction, IdentifyWithEnrolmentAction}
+import models.ReturnId
+import play.api.Logging
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class BusinessTaxAccountRedirect @Inject() (
+  config: FrontendAppConfig,
+  identify: IdentifyWithEnrolmentAction,
+  cacheConnector: CacheConnector,
+  dataRetrieval: DataRetrievalAction,
+  val controllerComponents: MessagesControllerComponents
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with Logging {
+
+  def onPageLoad: Action[AnyContent] = (identify andThen dataRetrieval).async { implicit request =>
+    request.session.get(periodKeySessionKey) match {
+      case Some(periodKey) =>
+        cacheConnector
+          .releaseLock(ReturnId(request.appaId, periodKey))
+          .map(_ => Redirect(config.businessTaxAccountUrl))
+      case None            =>
+        logger.info("Period key not found during sign out")
+        Future.successful(Redirect(config.businessTaxAccountUrl))
+    }
+  }
+
+}
---
+++ test/controllers/BusinessTaxAccountRedirectSpec.scala
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import config.FrontendAppConfig
+import connectors.CacheConnector
+import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchersSugar.eqTo
+import play.api.http.Status.SEE_OTHER
+import play.api.inject.bind
+import play.api.test.Helpers._
+
+import scala.concurrent.Future
+
+class BusinessTaxAccountRedirectSpec extends SpecBase {
+
+  "businessTaxAccount" - {
+
+    "if the period key is not available must redirect to the business tax account page" in {
+
+      val mockCacheConnector = mock[CacheConnector]
+
+      val application =
+        applicationBuilder(None)
+          .overrides(bind[CacheConnector].toInstance(mockCacheConnector))
+          .build()
+
+      running(application) {
+
+        val appConfig = application.injector.instanceOf[FrontendAppConfig]
+        val request   = FakeRequestWithoutSession(GET, controllers.routes.BusinessTaxAccountRedirect.onPageLoad().url)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual appConfig.businessTaxAccountUrl
+        verify(mockCacheConnector, times(0)).releaseLock(eqTo(returnId))(any())
+      }
+    }
+
+    "if the period key is retrieved, it must release the lock, and redirect to the business tax account page" in {
+
+      val mockCacheConnector = mock[CacheConnector]
+      when(mockCacheConnector.releaseLock(eqTo(returnId))(any())).thenReturn(Future.successful(()))
+
+      val application =
+        applicationBuilder(None)
+          .overrides(bind[CacheConnector].toInstance(mockCacheConnector))
+          .build()
+
+      running(application) {
+
+        val appConfig = application.injector.instanceOf[FrontendAppConfig]
+        val request   = FakeRequest(GET, controllers.routes.BusinessTaxAccountRedirect.onPageLoad().url)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual appConfig.businessTaxAccountUrl
+        verify(mockCacheConnector, times(1)).releaseLock(eqTo(returnId))(any())
+      }
+    }
+  }
+}
