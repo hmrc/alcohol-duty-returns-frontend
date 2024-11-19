7ff4a5b4c76f31f045977be4c852f39277aa65f0
simondrugan16 <119412400+simondrugan16@users.noreply.github.com>
2024-11-19 12:27:14 +0000

    ADR-1225 formatting

--- app/viewmodels/checkAnswers/spiritsQuestions/WhiskySummary.scala
+++ app/viewmodels/checkAnswers/spiritsQuestions/WhiskySummary.scala
@@ -30,8 +30,9 @@ object WhiskySummary {
     answers.get(WhiskyPage).map { answer =>
       SummaryListRowViewModel(
         key = "whisky.scotchWhisky.checkYourAnswersLabel",
-        value =
-          ValueViewModel(s"${messages("site.2DP", answer.scotchWhisky)} ${messages("spiritsQuestions.checkYourAnswers.lpa.units")}"),
+        value = ValueViewModel(
+          s"${messages("site.2DP", answer.scotchWhisky)} ${messages("spiritsQuestions.checkYourAnswers.lpa.units")}"
+        ),
         actions = Seq(
           ActionItemViewModel("site.change", routes.WhiskyController.onPageLoad(CheckMode).url)
             .withVisuallyHiddenText(messages("whisky.scotchWhisky.change.hidden"))
@@ -43,8 +44,9 @@ object WhiskySummary {
     answers.get(WhiskyPage).map { answer =>
       SummaryListRowViewModel(
         key = "whisky.irishWhiskey.checkYourAnswersLabel",
-        value =
-          ValueViewModel(s"${messages("site.2DP", answer.irishWhiskey)} ${messages("spiritsQuestions.checkYourAnswers.lpa.units")}"),
+        value = ValueViewModel(
+          s"${messages("site.2DP", answer.irishWhiskey)} ${messages("spiritsQuestions.checkYourAnswers.lpa.units")}"
+        ),
         actions = Seq(
           ActionItemViewModel("site.change", routes.WhiskyController.onPageLoad(CheckMode).url)
             .withVisuallyHiddenText(messages("whisky.irishWhiskey.change.hidden"))
