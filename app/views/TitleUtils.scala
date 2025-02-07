9472a1546f2210af4065ca26b0acc1903899ef79
ajchilts <40767309+ajchilts@users.noreply.github.com>
2025-02-07 10:18:25 +0000

    ADR-1800 | Fix error prefix for Welsh language toggle

--- app/views/TitleUtils.scala
+++ app/views/TitleUtils.scala
@@ -21,22 +21,15 @@ import play.api.i18n.Messages
 
 object TitleUtils {
 
-  /**
-    * Call this one if you have a validated form on your page since it'll prefix the browser title with Error: if one
-    *
-    * title should already been looked up
-    */
-  def title(form: Form[_], title: String)(implicit messages: Messages): String =
-    titleNoForm(s"${errorPrefix(form)} $title")
+  def title(form: Form[_], title: String, section: Option[String] = None)(implicit messages: Messages): String =
+    titleNoForm(
+      title = s"${errorPrefix(form)} ${messages(title)}",
+      section = section
+    )
 
-  /**
-    * Call this one on a page without a validated form
-    *
-    * title should already been looked up
-    */
-  def titleNoForm(title: String)(implicit messages: Messages): String =
-    s"$title - ${messages("service.name")} - ${messages("site.govuk")}"
+  def titleNoForm(title: String, section: Option[String] = None)(implicit messages: Messages): String =
+    s"${messages(title)} - ${section.fold("")(messages(_) + " - ")}${messages("service.name")} - ${messages("site.govuk")}"
 
   private def errorPrefix(form: Form[_])(implicit messages: Messages): String =
-    if (form.hasErrors || form.hasGlobalErrors) messages("error.browser.title.prefix") else ""
+    if (form.hasErrors || form.hasGlobalErrors) messages("error.title.prefix") else ""
 }
