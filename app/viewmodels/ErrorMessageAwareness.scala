5f6307647bc9c023cfd621badd7e0f97e3ccdf5f
ajchilts <40767309+ajchilts@users.noreply.github.com>
2025-02-10 11:36:25 +0000

    ADR-1800 | Remove redudant code

--- app/viewmodels/ErrorMessageAwareness.scala
+++ app/viewmodels/ErrorMessageAwareness.scala
@@ -26,7 +26,6 @@ trait ErrorMessageAwareness {
   def errorMessage(field: Field)(implicit messages: Messages): Option[ErrorMessage] =
     field.error
       .map { err =>
-        ErrorMessage(content = Text(messages(err.message, err.args: _*)))
         ErrorMessage(
           content = Text(messages(err.message, err.args: _*)),
           visuallyHiddenText = Some(messages("error.prefix"))
