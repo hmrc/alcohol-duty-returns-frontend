378012152ee2f1dc6a2aee024b0a2af103eb0a26
matthew-goh <186605436+matthew-goh@users.noreply.github.com>
2025-03-13 11:08:55 +0000

    ADR-1899 Update library dependencies/plugins, addressing PR bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.9.0"
-  private val hmrcMongoVersion = "2.5.0"
+  private val bootstrapVersion = "9.11.0"
+  private val hmrcMongoVersion = "2.6.0"
   val mockitoScalaVersion = "1.17.37"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "11.11.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "11.12.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.2.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"     % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"             % hmrcMongoVersion,
