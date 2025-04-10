b6c4ec9e78fec2ef1bb100c7395f8e8be9f324f0
matthew-goh <186605436+matthew-goh@users.noreply.github.com>
2025-04-10 14:25:59 +0100

    ADR-1606 First page of new DSD journey

---
+++ app/controllers/dutySuspendedNew/DeclareDutySuspendedDeliveriesQuestionNewController.scala
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.dutySuspendedNew
+
+import connectors.UserAnswersConnector
+import controllers.actions._
+import forms.dutySuspendedNew.DeclareDutySuspendedDeliveriesQuestionNewFormProvider
+import models.{Mode, UserAnswers}
+import navigation.DeclareDutySuspendedDeliveriesNewNavigator
+import pages.dutySuspendedNew.{DeclareDutySuspendedDeliveriesQuestionNewPage, DutySuspendedAlcoholTypePage}
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.dutySuspendedNew.DeclareDutySuspendedDeliveriesQuestionNewView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
+
+class DeclareDutySuspendedDeliveriesQuestionNewController @Inject() (
+  override val messagesApi: MessagesApi,
+  userAnswersConnector: UserAnswersConnector,
+  navigator: DeclareDutySuspendedDeliveriesNewNavigator,
+  identify: IdentifyWithEnrolmentAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  checkDSDNewJourneyToggle: CheckDSDNewJourneyToggleAction,
+  formProvider: DeclareDutySuspendedDeliveriesQuestionNewFormProvider,
+  val controllerComponents: MessagesControllerComponents,
+  view: DeclareDutySuspendedDeliveriesQuestionNewView
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with I18nSupport {
+
+  val form = formProvider()
+
+  def onPageLoad(mode: Mode): Action[AnyContent] =
+    (identify andThen getData andThen requireData andThen checkDSDNewJourneyToggle) { implicit request =>
+      val preparedForm = request.userAnswers.get(DeclareDutySuspendedDeliveriesQuestionNewPage) match {
+        case None        => form
+        case Some(value) => form.fill(value)
+      }
+
+      Ok(view(preparedForm, mode))
+    }
+
+  def onSubmit(mode: Mode): Action[AnyContent] =
+    (identify andThen getData andThen requireData andThen checkDSDNewJourneyToggle).async { implicit request =>
+      form
+        .bindFromRequest()
+        .fold(
+          formWithErrors => Future.successful(BadRequest(view(formWithErrors, mode))),
+          value =>
+            for {
+              updatedAnswers                <-
+                Future.fromTry(
+                  request.userAnswers.set(DeclareDutySuspendedDeliveriesQuestionNewPage, value)
+                )
+              singleRegimeUpdatedUserAnswer <- Future.fromTry(checkIfOneRegimeAndUpdateUserAnswer(updatedAnswers))
+              filterUserAnswer              <- Future.fromTry(filterDSDQuestionAnswer(singleRegimeUpdatedUserAnswer, value))
+              _                             <- userAnswersConnector.set(filterUserAnswer)
+            } yield Redirect(
+              navigator.nextPage(DeclareDutySuspendedDeliveriesQuestionNewPage, mode, filterUserAnswer, Some(false))
+            )
+        )
+    }
+
+  private def checkIfOneRegimeAndUpdateUserAnswer(userAnswer: UserAnswers): Try[UserAnswers] =
+    if (userAnswer.regimes.regimes.size == 1) {
+      userAnswer.set(DutySuspendedAlcoholTypePage, userAnswer.regimes.regimes)
+    } else {
+      Try(userAnswer)
+    }
+
+  private def filterDSDQuestionAnswer(userAnswer: UserAnswers, value: Boolean): Try[UserAnswers] =
+    if (value) {
+      Try(userAnswer)
+    } else {
+      // TODO: add other pages from new journey to be removed if user submits 'No'
+      userAnswer.remove(
+        List(
+          DutySuspendedAlcoholTypePage
+        )
+      )
+    }
+}
---
+++ app/navigation/DeclareDutySuspendedDeliveriesNewNavigator.scala
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package navigation
+
+import controllers._
+import models._
+import pages._
+import pages.dutySuspendedNew._
+import play.api.mvc.Call
+
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class DeclareDutySuspendedDeliveriesNewNavigator @Inject() () {
+
+  private val normalRoutes: Page => UserAnswers => Call = {
+    case DeclareDutySuspendedDeliveriesQuestionNewPage =>
+      userAnswers => declareDutySuspendedDeliveriesQuestionPageRoute(userAnswers, NormalMode)
+    case DutySuspendedAlcoholTypePage                  =>
+      // TODO: Implement for alcohol type selection page
+      _ => routes.TaskListController.onPageLoad
+  }
+
+  private val normalRoutesWithRegime: Page => UserAnswers => AlcoholRegime => Call =
+    // TODO: Implement for new pages that depend on the regime
+    _ => _ => _ => routes.TaskListController.onPageLoad
+
+  private val checkRouteMap: Page => UserAnswers => Boolean => Call = {
+    case DeclareDutySuspendedDeliveriesQuestionNewPage =>
+      userAnswers => _ => declareDutySuspendedDeliveriesQuestionPageRoute(userAnswers, CheckMode)
+    case DutySuspendedAlcoholTypePage                  =>
+      // TODO: Implement for alcohol type selection page
+      _ => _ => routes.TaskListController.onPageLoad
+  }
+
+  private val checkRouteMapWithRegime: Page => UserAnswers => AlcoholRegime => Call =
+    // TODO: Implement for new pages that depend on the regime (always go to Check Your Answers)
+    _ => _ => _ => routes.TaskListController.onPageLoad
+
+  def nextPageWithRegime(
+    page: Page,
+    mode: Mode,
+    userAnswers: UserAnswers,
+    regime: AlcoholRegime
+  ): Call = mode match {
+    case NormalMode =>
+      normalRoutesWithRegime(page)(userAnswers)(regime)
+    case CheckMode  =>
+      checkRouteMapWithRegime(page)(userAnswers)(regime)
+  }
+
+  def nextPage(
+    page: Page,
+    mode: Mode,
+    userAnswers: UserAnswers,
+    hasAnswerChanged: Option[Boolean]
+  ): Call = mode match {
+    case NormalMode =>
+      normalRoutes(page)(userAnswers)
+    case CheckMode  =>
+      checkRouteMap(page)(userAnswers)(hasAnswerChanged.getOrElse(false))
+  }
+
+  private def declareDutySuspendedDeliveriesQuestionPageRoute(userAnswers: UserAnswers, mode: Mode): Call =
+    userAnswers.get(DeclareDutySuspendedDeliveriesQuestionNewPage) match {
+      case Some(true) if userAnswers.regimes.regimes.size > 1 =>
+        // TODO: Go to new declare quantity page for the single regime (insert mode on page load)
+        routes.TaskListController.onPageLoad
+      case Some(true)                                         =>
+        // TODO: Go to new alcohol type selection page (insert mode on page load)
+        routes.TaskListController.onPageLoad
+      case Some(false)                                        => routes.TaskListController.onPageLoad
+      case _                                                  => routes.JourneyRecoveryController.onPageLoad()
+    }
+}
