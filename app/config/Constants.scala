964750ba68a87a1d914435e3446836b2d78478dd
Bradleigh-L <189644673+Bradleigh-L@users.noreply.github.com>
2025-06-13 08:36:07 +0100

    ADR-2093: Add to constants and use

--- app/config/Constants.scala
+++ app/config/Constants.scala
@@ -157,6 +157,7 @@ object Constants {
 
     val marginTop8CssClass    = "govuk-!-margin-top-8"
     val marginBottom0CssClass = "govuk-!-margin-bottom-0"
+    val marginBottom1CssClass = "govuk-!-margin-bottom-1"
     val marginBottom8CssClass = "govuk-!-margin-bottom-8"
 
     val paddingBottomCssClass = "govuk-body govuk-!-padding-bottom-3"
