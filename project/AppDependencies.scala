d4def4d27138d822f9d5728d88843c9d1658bc70
Bradleigh-L <189644673+Bradleigh-L@users.noreply.github.com>
2025-07-25 16:52:29 +0100

    ADR-2125: Rollback bootstrapVersion to 9.16 due to test failures

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.17.0"
+  private val bootstrapVersion = "9.16.0"
   private val hmrcMongoVersion = "2.7.0"
   val mockitoScalaVersion = "1.17.37"
 
