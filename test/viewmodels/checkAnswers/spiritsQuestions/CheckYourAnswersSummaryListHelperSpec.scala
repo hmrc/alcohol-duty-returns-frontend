b31256e83378fc0ed9e0b03881e9a945180b753e
matthew-goh <186605436+matthew-goh@users.noreply.github.com>
2025-10-23 14:45:47 +0100

    ADR-2301 Quarterly spirits CYA

--- test/viewmodels/checkAnswers/spiritsQuestions/CheckYourAnswersSummaryListHelperSpec.scala
+++ test/viewmodels/checkAnswers/spiritsQuestions/CheckYourAnswersSummaryListHelperSpec.scala
@@ -17,19 +17,16 @@
 package viewmodels.checkAnswers.spiritsQuestions
 
 import base.SpecBase
-import models.SpiritType
 import models.spiritsQuestions.Whisky
+import models.{CheckMode, SpiritType}
 import pages.spiritsQuestions.{DeclareSpiritsTotalPage, OtherSpiritsProducedPage, SpiritTypePage, WhiskyPage}
 import uk.gov.hmrc.govukfrontend.views.Aliases.Text
-import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.{Card, CardTitle}
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.ActionItem
 
 class CheckYourAnswersSummaryListHelperSpec extends SpecBase {
 
   val completedUserAnswers = emptyUserAnswers
-    .set(
-      SpiritTypePage,
-      Set[SpiritType](SpiritType.Other)
-    )
+    .set(SpiritTypePage, Set[SpiritType](SpiritType.Other))
     .success
     .value
     .set(OtherSpiritsProducedPage, "Coco Pops")
@@ -42,18 +39,60 @@ class CheckYourAnswersSummaryListHelperSpec extends SpecBase {
     .success
     .value
 
+  val urlBase = "/manage-alcohol-duty"
+
+  val expectedBaseActions = Seq(
+    ActionItem(
+      content = Text("Change"),
+      href = urlBase + controllers.spiritsQuestions.routes.DeclareSpiritsTotalController.onPageLoad(CheckMode).url,
+      visuallyHiddenText = Some("total of all spirits")
+    ),
+    ActionItem(
+      content = Text("Change"),
+      href = urlBase + controllers.spiritsQuestions.routes.WhiskyController.onPageLoad(CheckMode).url,
+      visuallyHiddenText = Some("Scotch Whisky")
+    ),
+    ActionItem(
+      content = Text("Change"),
+      href = urlBase + controllers.spiritsQuestions.routes.WhiskyController.onPageLoad(CheckMode).url,
+      visuallyHiddenText = Some("Irish Whiskey")
+    ),
+    ActionItem(
+      content = Text("Change"),
+      href = urlBase + controllers.spiritsQuestions.routes.SpiritTypeController.onPageLoad(CheckMode).url,
+      visuallyHiddenText = Some("type of spirits")
+    )
+  )
+
+  val expectedFullActions = expectedBaseActions :+ ActionItem(
+    content = Text("Change"),
+    href = urlBase + controllers.spiritsQuestions.routes.OtherSpiritsProducedController.onPageLoad(CheckMode).url,
+    visuallyHiddenText = Some("other spirits produced")
+  )
+
   "CheckYourAnswersSummaryListHelper" - {
 
     "must return a summary list with the correct rows if pages are populated (Other spirits is selected)" in {
       val expectedSummaryListKeys =
         List("Total of all spirits", "Scotch Whisky", "Irish Whiskey", "Type of spirits", "Other spirits produced")
 
+      val expectedSummaryListValues = List(
+        "11.00 litres of pure alcohol",
+        "1.00 litres of pure alcohol",
+        "2.00 litres of pure alcohol",
+        "<span aria-label='Type of spirits'><span class='break'>Other spirits</span>",
+        "Coco Pops"
+      )
+
       val summaryList = CheckYourAnswersSummaryListHelper
         .spiritsSummaryList(completedUserAnswers)(getMessages(app))
         .getOrElse(fail("Expected a Some containing a summary list"))
 
-      summaryList.rows.map(_.key.content.asHtml.toString) mustBe expectedSummaryListKeys
-      summaryList.card                                    mustBe Some(Card(Some(CardTitle(Text("Spirits you’ve produced")))))
+      summaryList.rows.map(_.key.content.asHtml.toString)   mustBe expectedSummaryListKeys
+      summaryList.rows.map(_.value.content.asHtml.toString) mustBe expectedSummaryListValues
+      summaryList.rows.map(_.actions.get.items.head)        mustBe expectedFullActions
+      summaryList.card.get.title.get.content                mustBe Text("Spirits you’ve produced")
+      summaryList.card.get.actions                          mustBe None
     }
 
     "must return a summary list with the correct rows if all required pages are populated (Other spirits is not selected)" in {
@@ -61,21 +100,28 @@ class CheckYourAnswersSummaryListHelperSpec extends SpecBase {
         .remove(List(OtherSpiritsProducedPage, SpiritTypePage))
         .success
         .value
-        .set(
-          SpiritTypePage,
-          Set[SpiritType](SpiritType.Maltspirits, SpiritType.Grainspirits)
-        )
+        .set(SpiritTypePage, Set[SpiritType](SpiritType.Maltspirits, SpiritType.Grainspirits))
         .success
         .value
 
       val expectedSummaryListKeys = List("Total of all spirits", "Scotch Whisky", "Irish Whiskey", "Type of spirits")
 
+      val expectedSummaryListValues = List(
+        "11.00 litres of pure alcohol",
+        "1.00 litres of pure alcohol",
+        "2.00 litres of pure alcohol",
+        "<span aria-label='Type of spirits'><span class='break'>Malt spirits,</span><span class='break'>Grain spirits</span>"
+      )
+
       val summaryList = CheckYourAnswersSummaryListHelper
         .spiritsSummaryList(userAnswersWithoutOtherSpiritsSelected)(getMessages(app))
         .getOrElse(fail("Expected a Some containing a summary list"))
 
-      summaryList.rows.map(_.key.content.asHtml.toString) mustBe expectedSummaryListKeys
-      summaryList.card                                    mustBe Some(Card(Some(CardTitle(Text("Spirits you’ve produced")))))
+      summaryList.rows.map(_.key.content.asHtml.toString)   mustBe expectedSummaryListKeys
+      summaryList.rows.map(_.value.content.asHtml.toString) mustBe expectedSummaryListValues
+      summaryList.rows.map(_.actions.get.items.head)        mustBe expectedBaseActions
+      summaryList.card.get.title.get.content                mustBe Text("Spirits you’ve produced")
+      summaryList.card.get.actions                          mustBe None
     }
 
     "must return None if a required page is not populated" - {
