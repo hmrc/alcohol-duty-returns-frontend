95f3b12ff87863a57af1f44bd3ac418e098731d8
matthew-goh <186605436+matthew-goh@users.noreply.github.com>
2025-11-05 13:31:10 +0000

    ADR-2332 Version updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.3.0"
+  private val bootstrapVersion = "10.4.0"
   private val hmrcMongoVersion = "2.10.0"
   val mockitoScalaVersion = "2.0.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "12.19.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "12.20.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"     % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"             % hmrcMongoVersion,
