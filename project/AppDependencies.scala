95765bcdb5d71592099d43c3162069ea205db452
ajchilts <40767309+ajchilts@users.noreply.github.com>
2025-12-03 15:07:25 +0000

    ADR-2296 | Address PR bot comment

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "12.20.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "12.21.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"     % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"             % hmrcMongoVersion,
