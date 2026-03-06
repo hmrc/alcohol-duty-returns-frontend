178ef39b4d2bf3c8c3580fe4789fb90573bad5a3
Lorenzo Mugnai <11977337+lmugnai@users.noreply.github.com>
2026-03-06 12:13:55 +0000

    Update deps + plugins to address security vulnerabilities (#464)
    * Update deps + plugins to address security vulnerabilities and Update copyright dates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,22 +2,22 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.7.0"
   private val hmrcMongoVersion = "2.12.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "12.29.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "12.32.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.4.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"     % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"             % hmrcMongoVersion,
     "org.typelevel"     %% "cats-core"                      % "2.13.0",
-    "com.beachape"      %% "enumeratum"                     % "1.9.0",
-    "com.beachape"      %% "enumeratum-play"                % "1.9.0",
-    "com.beachape"      %% "enumeratum-cats"                % "1.9.0"
+    "com.beachape"      %% "enumeratum"                     % "1.9.5",
+    "com.beachape"      %% "enumeratum-play"                % "1.9.5",
+    "com.beachape"      %% "enumeratum-cats"                % "1.9.5"
   )
 
-  val test = Seq(
+  val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
     "org.scalatest"           %% "scalatest"               % "3.2.19",
@@ -28,6 +28,6 @@ object AppDependencies {
     "com.vladsch.flexmark"    %  "flexmark-all"            % "0.64.8"
   ).map(_ % Test)
 
-  val itDependencies = Seq.empty
+  val itDependencies: Seq[Nothing] = Seq.empty
   def apply(): Seq[ModuleID] = compile ++ test
 }
