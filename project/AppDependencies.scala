70a35a0af00b7d72cc455e14ff3be07aed7dddec
tolgatunaaa <107684014+tolgatunaaa@users.noreply.github.com>
2026-05-27 12:18:18 +0100

    DDCE-8147 - Add and run scalafmtAll

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,27 +7,27 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "12.32.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.5.0",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"     % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"             % hmrcMongoVersion,
-    "org.typelevel"     %% "cats-core"                      % "2.13.0",
-    "com.beachape"      %% "enumeratum"                     % "1.9.7",
-    "com.beachape"      %% "enumeratum-play"                % "1.9.7",
-    "com.beachape"      %% "enumeratum-cats"                % "1.9.7"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.32.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
+    "org.typelevel"     %% "cats-core"                             % "2.13.0",
+    "com.beachape"      %% "enumeratum"                            % "1.9.7",
+    "com.beachape"      %% "enumeratum-play"                       % "1.9.7",
+    "com.beachape"      %% "enumeratum-cats"                       % "1.9.7"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
-    "org.scalatest"           %% "scalatest"               % "3.2.19",
-    "org.scalatestplus"       %% "scalacheck-1-17"         % "3.2.18.0",
-    "com.beachape"            %% "enumeratum-scalacheck"   % "1.9.7",
-    "org.scalacheck"          %% "scalacheck"              % "1.19.0",
-    "org.jsoup"               %  "jsoup"                   % "1.21.2",
-    "com.vladsch.flexmark"    %  "flexmark-all"            % "0.64.8"
+    "uk.gov.hmrc"         %% "bootstrap-test-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo"   %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
+    "org.scalatest"       %% "scalatest"               % "3.2.19",
+    "org.scalatestplus"   %% "scalacheck-1-17"         % "3.2.18.0",
+    "com.beachape"        %% "enumeratum-scalacheck"   % "1.9.7",
+    "org.scalacheck"      %% "scalacheck"              % "1.19.0",
+    "org.jsoup"            % "jsoup"                   % "1.21.2",
+    "com.vladsch.flexmark" % "flexmark-all"            % "0.64.8"
   ).map(_ % Test)
 
   val itDependencies: Seq[Nothing] = Seq.empty
-  def apply(): Seq[ModuleID] = compile ++ test
+  def apply(): Seq[ModuleID]       = compile ++ test
 }
