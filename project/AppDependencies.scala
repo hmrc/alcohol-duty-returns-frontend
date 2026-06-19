ca65115eec3ce9ec80a2e7c91bcf01708b6c7988
Shamanth.Rajulapati <204857173+Shamanth9798@users.noreply.github.com>
2026-06-19 10:57:02 +0100

    DDCE-8166: Updated Readme file  (#468)
    * Updated Readme file for Payment journey and addressed some Dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.7.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.9.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
@@ -20,11 +20,11 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"         %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
-    "org.scalatest"       %% "scalatest"               % "3.2.19",
+    "org.scalatest"       %% "scalatest"               % "3.2.20",
     "org.scalatestplus"   %% "scalacheck-1-17"         % "3.2.18.0",
     "com.beachape"        %% "enumeratum-scalacheck"   % "1.9.7",
     "org.scalacheck"      %% "scalacheck"              % "1.19.0",
-    "org.jsoup"            % "jsoup"                   % "1.21.2",
+    "org.jsoup"            % "jsoup"                   % "1.22.2",
     "com.vladsch.flexmark" % "flexmark-all"            % "0.64.8"
   ).map(_ % Test)
 
