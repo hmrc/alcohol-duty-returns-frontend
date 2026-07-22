3950bb4f12d8cc57d175b8ad541b46f1fea96ad4
lmugnai <11977337+lmugnai@users.noreply.github.com>
2026-07-22 12:24:36 +0100

    Align service navigation with PlatUI servcies + dep updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "10.8.0"
-  private val hmrcMongoVersion = "2.12.0"
+  private val hmrcMongoVersion = "2.13.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
